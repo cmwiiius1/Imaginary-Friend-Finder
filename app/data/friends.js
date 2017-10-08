f3f004ea845e16123314d93a0d53c6fb86cbe82f
cmwiiius1 <cmwiiius1@hotmail.com>
2017-10-08 07:14:25 -0400

    Final IFF

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+// data on the friends
+var friends = [{
+	"name":"Eeyore",
+	"photo":"https://i.pinimg.com/736x/e1/58/34/e158347c812d54feb157f39fb6bcf886--pooh-bear-disney-characters.jpg",
+	"scores":[1, 1, 1, 1, 1, 1, 2, 5, 1, 1]
+}, {
+	"name":"Dopey",
+	"photo":"https://i.pinimg.com/736x/2b/47/0e/2b470ea4138dd20f9d8e275e1cca0017--drawing-cartoons-crochet-motifs.jpg",
+	"scores":[5, 1, 2, 2, 2, 5, 3, 4, 2, 4]	
+}, {
+	"name":"Sebastian",
+	"photo":"http://images.clipartpanda.com/little-mermaid-clip-art-little-mermaid-clip-art-3.jpg",
+	"scores":[2, 5, 5, 5, 4, 1, 3, 4, 3, 3]
+}, {
+	"name":"Minnie Mouse",
+	"photo":"https://i.pinimg.com/736x/dc/50/af/dc50afcdb616a0e552dece8033c24804--cartoon-picture-vintage-cartoons.jpg",
+	"scores":[5, 5, 1, 3, 1, 5, 1, 5, 5, 3]
+}, {
+	"name":"Goofey",
+	"photo":"https://vignette3.wikia.nocookie.net/fantendo/images/6/60/DP_Goofy.jpg/revision/latest?cb=20150202040836",
+	"scores":[5, 5, 1, 3, 1, 5, 1, 5, 5, 5]
+}, {
+	"name":"Donald Duck",
+	"photo":"http://images2.fanpop.com/images/photos/6000000/Donald-Duck-donald-duck-6042600-469-500.jpg",
+	"scores":[2, 2, 4, 4, 3, 2, 3, 2, 2, 4]
+}, {
+	"name":"Daisy Duck",
+	"photo":"http://stuffpoint.com/disney/image/271797-disney-daisy-duck.png",
+	"scores":[2, 5, 4, 4, 3, 2, 3, 2, 2, 4]
+}, {
+	"name":"Flower",
+	"photo":"https://vignette2.wikia.nocookie.net/disney/images/c/cc/Bambi-aaa0ee2c.jpg/revision/latest?cb=20120804072144",
+	"scores":[3, 3, 3, 4, 3, 3, 3, 2, 2, 4]
+}, {
+	"name":"Mickey Mouse",
+	"photo":"https://i.pinimg.com/originals/bc/e8/1c/bce81c6a9fa8e291f4548727631ec3fd.jpg",
+	"scores":[4, 3, 4, 1, 5, 1, 4, 3, 3, 5]
+}];
+
+module.exports = friends;
